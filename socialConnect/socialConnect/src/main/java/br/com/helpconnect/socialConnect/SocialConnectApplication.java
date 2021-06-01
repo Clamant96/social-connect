d36f56d7e127806f510c0371a300c184b33bad0a
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-05-31 22:23:11 -0300

    Importacao de projeto.

---
+++ socialConnect/socialConnect/src/main/java/br/com/helpconnect/socialConnect/SocialConnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.helpconnect.socialConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialConnectApplication.class, args);
+	}
+
+}
---
+++ socialConnect/socialConnect/src/test/java/br/com/helpconnect/socialConnect/SocialConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helpconnect.socialConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
